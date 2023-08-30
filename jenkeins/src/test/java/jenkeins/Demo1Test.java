b9dfc3351d0b363f7445f897d867467a233059db
m-a-firdous <afeefafirdous987@gmail.com>
2023-08-30 07:46:59 +0530

    1st push

---
+++ jenkeins/src/test/java/jenkeins/Demo1Test.java
@@ -0,0 +1,13 @@
+package jenkeins;
+
+import org.testng.annotations.Test;
+
+public class Demo1Test
+{
+	@Test
+	public void Demo1()
+	{
+		System.out.println("hello");
+	}
+
+}
---
+++ jenkeins/src/test/java/jenkeins/Demo2Test.java
@@ -0,0 +1,14 @@
+package jenkeins;
+
+import org.testng.annotations.Test;
+
+public class Demo2Test
+{
+	@Test
+	public void Demo2()
+	{
+		System.out.println("hi");
+
+	}
+
+}
